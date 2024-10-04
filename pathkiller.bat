113158a8a4e6f4b7d3c8f0728e6376f14a05e2c6
Freenitial <83383164+Freenitial@users.noreply.github.com>
2024-10-05 01:43:41 +0200

    Update pathkiller.bat

--- pathkiller.bat
+++ pathkiller.bat
@@ -1,10 +1,11 @@
-@echo off & chcp 65001 >nul & setlocal
+@echo off & chcp 65001 >nul & setlocal & echo.
 
 
 
-::------------------------------------------------------------::
-::    Kill any process located into specified folder paths    ::
-::------------------------------------------------------------::
+echo -------------------   PATH KILLER   ------------------------
+                                                              
+REM      Kill any process located into specified folder paths    
+REM  ------------------------------------------------------------
 
 
 
@@ -18,7 +19,7 @@ REM If you call without any arg : full default config below applied
 
 REM ================= DEFAULT CONFIGURATION ====================
 
-set "folders="C:\first\path";"C:\second path""     :: List of paths formated like this
+set "folders="C:\Program Files\Google\Chrome\Application";"C:\second path""     :: List of paths formated like this
 set "recursive=1"                                  :: Enable recursion into subfolders
 set "testing=0"                                    :: Only display, not kill
 
@@ -41,16 +42,11 @@ goto :parse_args
 
 REM ===================== EXECUTION =============================
 
-if not defined folders (
-    echo No folders provided.
-    timeout /t 2 >nul
-    exit /b 0
-)
+if not defined folders (echo Error - %%folders%% variable is null & timeout /t 2 >nul & set "returncode=1" & goto :end)
 
 REM Create the PowerShell filter logic dynamically based on the folder list
 setlocal enabledelayedexpansion
 set "filter="
-
 for %%F in (%folders%) do (
     set "tempFolder=%%F" & set "tempFolder=!tempFolder:~1,-1!"
     if "%recursive%"=="1" (
@@ -59,26 +55,31 @@ for %%F in (%folders%) do (
         set "filter=!filter!($_.ExecutablePath -like '!tempFolder!\*' -and (Split-Path $_.ExecutablePath -Parent) -eq '!tempFolder!') -or "
     )
 )
-
 REM Remove the trailing " -or " from the filter string
 set "filter=%filter:~0,-4%"
+if not defined filter (echo No valid folder filter created. & timeout /t 2 >nul & set "returncode=2" & goto :end)
 
-if not defined filter (
-    echo No valid folder filter created.
-    timeout /t 2 >nul
-    exit /b 0
-)
-
+echo Looking for processes to kill...
 REM (Compatibility) Using "WMI" below instead of "Get-Process" to keep all user processes visible by system account / SCCM
 if "%testing%"=="1" (
-    echo Testing mode is enabled. No action will be performed.
-    echo %%filter%% = %filter%
+    :: echo _debug_ %%filter%% = %filter%
     powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-        "Get-WmiObject Win32_Process | Where-Object { %filter% } | Select-Object -Property ProcessId, Name, ExecutablePath"
+        "try{((Get-WmiObject Win32_Process | Where-Object { %filter% } | Select-Object -Property ProcessId, Name, ExecutablePath)[0])}catch{exit 1}" ^
+         || echo -testing mode- NOT any matching process found. && set "returncode=1"
     pause
 ) else (
     powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-        "Get-WmiObject Win32_Process | Where-Object { %filter% } | ForEach-Object { taskkill /PID $_.ProcessId /F }"
+        "try{((Get-WmiObject Win32_Process | Where-Object { %filter% } | ForEach-Object { taskkill /PID $_.ProcessId /F })[0])}catch{exit 1}" >nul ^
+         || echo NOT any matching process found. && set "returncode=1"
 )
 
+
+
+REM ====================== ENDING ===============================
+
+:end
+if not defined returncode set "returncode=0"
+::echo _debug_  %%returncode%% = %returncode% & pause
+echo ------------------------------------------------------------ & echo.
 endlocal & exit /b 0
+REM You can do "exit /b %returncode%" to handle cases
